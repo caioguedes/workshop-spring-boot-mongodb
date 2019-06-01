6040c297d33eb4180422a85790a917fe7ab6576b
Caio Guedes <caio.cesar.g.souza@gmail.com>
2019-06-01 15:46:07 -0300

    Initial files

---
+++ src/main/java/com/caioguedes/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.caioguedes.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopmongoApplication.class, args);
+    }
+
+}
